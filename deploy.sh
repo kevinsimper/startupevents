41d1377e15a7860227a628885dd6d150bcab1d42
Kevin Simper <kevin.simper@gmail.com>
2016-09-28 22:52:28 +0200

    hyper pull

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,9 @@
 NEWNAME=startupevents$RANDOM
 NEWCONTAINER=$NEWNAME-web-1
 IP="209.177.85.124"
+IMAGE="kevinsimper/startupevents:latest"
+hyper pull $IMAGE
 hyper compose up -f hyper-compose.yml -d -p $NEWNAME
-OLD=$(hyper fip ls | grep 209.177.85.124 | awk '{print $2}')
+OLD=$(hyper fip ls | grep $IP | awk '{print $2}')
 hyper fip detach $OLD
 hyper fip attach $IP $NEWCONTAINER
