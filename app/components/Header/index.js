e68d7de2ef1bef81d877ffc29abe461bd63b87d2
Kevin Simper <kevin.simper@gmail.com>
2016-10-01 01:10:13 +0200

    Add header component

--- app/components/Header/index.js
+++ app/components/Header/index.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react'
+import styles from './style.scss'
 
 export default class Header extends Component {
   render() {
     return (
       <div>
-        <h1>Startup Events in Copenhagen</h1>
+        <h1 className={styles.Header}>Startup Events in Copenhagen</h1>
       </div>
     )
   }
