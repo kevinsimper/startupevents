13673a820e9febdb99a1d9f85e2177fe6539d838
Kevin Simper <kevin.simper@gmail.com>
2016-10-01 01:42:58 +0200

    Show better time and date for each event

--- app/components/Event/index.js
+++ app/components/Event/index.js
@@ -1,13 +1,18 @@
 import React, { Component } from 'react'
 import styles from './style.scss'
+import moment from 'moment'
 
 export default class Event extends Component {
   render() {
     const { event } = this.props
+    let start = moment(event.start)
+    let end = moment(event.end)
     return (
       <div className={styles.Event}>
         <h3>{event.title}</h3>
-        <div>{event.start.toString()} - {event.end.toString()}</div>
+        <div>
+          <strong>{start.format('DD/MM | HH:mm')} - {end.format('HH:mm')}</strong>
+        </div>
         <div>{event.description}</div>
       </div>
     )
