6347129b3be90e7affda3b8dde120229b41d7d03
Kevin Simper <kevin.simper@gmail.com>
2016-10-01 01:02:35 +0200

    Add event style file

--- app/components/Event/index.js
+++ app/components/Event/index.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react'
+import styles from './style.scss'
 
 export default class Event extends Component {
   render() {
     const { event } = this.props
     return (
-      <div>
+      <div className={styles.Event}>
         <h3>{event.title}</h3>
         <div>{event.start.toString()} - {event.end.toString()}</div>
         <div>{event.description}</div>
