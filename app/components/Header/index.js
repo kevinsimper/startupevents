17816f41bb0e7152fcb6af5617fae2a7fc4d192c
Kevin Simper <kevin.simper@gmail.com>
2016-10-01 02:45:48 +0200

    Colorful header

--- app/components/Header/index.js
+++ app/components/Header/index.js
@@ -4,7 +4,7 @@ import styles from './style.scss'
 export default class Header extends Component {
   render() {
     return (
-      <div>
+      <div className={styles.Container}>
         <h1 className={styles.Header}>Startup Events in Copenhagen</h1>
       </div>
     )
