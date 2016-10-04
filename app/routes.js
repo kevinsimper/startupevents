d20a77c3258cd9ed9617d99a903ae150efbe4da7
Kevin Simper <kevin.simper@gmail.com>
2016-10-05 01:29:22 +0200

    Login component

--- app/routes.js
+++ app/routes.js
@@ -2,10 +2,12 @@ import React from 'react'
 import { Route, IndexRoute } from 'react-router'
 import Main from './components/Main'
 import HelloWorld from './containers/HelloWorld'
+import Login from './containers/Login'
 
 export default (
   <Route path='/' component={Main}>
     <IndexRoute component={HelloWorld}/>
     <Route path='date/:year/:month/:day' component={HelloWorld}/>
+    <Route path='login' component={Login}/>
   </Route>
 )
