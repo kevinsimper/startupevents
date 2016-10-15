42622431888ed476c8009944c072c679f81d4a14
Kevin Simper <kevin.simper@gmail.com>
2016-10-15 23:26:14 +0200

    Add admin page

--- app/routes.js
+++ app/routes.js
@@ -3,11 +3,13 @@ import { Route, IndexRoute } from 'react-router'
 import Main from './components/Main'
 import HelloWorld from './containers/HelloWorld'
 import Login from './containers/Login'
+import Admin from './containers/Admin'
 
 export default (
   <Route path='/' component={Main}>
     <IndexRoute component={HelloWorld}/>
     <Route path='date/:year/:month/:day' component={HelloWorld}/>
     <Route path='login' component={Login}/>
+    <Route path='admin' component={Admin}/>
   </Route>
 )
