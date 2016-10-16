706200f897affb62ed2aa5dcb726085cc00b85fb
Kevin Simper <kevin.simper@gmail.com>
2016-10-16 12:43:32 +0200

    Add the about page

---
+++ app/containers/About.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+export default class About extends Component {
+  render() {
+    return (
+      <div>
+        <h1>About</h1>
+        <p>
+          We wanted a place where you could find all events in copenhagen and where you could find more information about each event, something that meetup.com does not provide.
+        </p>
+      </div>
+    )
+  }
+}
--- app/routes.js
+++ app/routes.js
@@ -5,12 +5,14 @@ import HelloWorld from './containers/HelloWorld'
 import Login from './containers/Login'
 import Admin from './containers/Admin'
 import AdminEvent from './containers/AdminEvent'
+import About from './containers/About'
 
 export default (
   <Route path='/' component={Main}>
     <IndexRoute component={HelloWorld}/>
     <Route path='date/:year/:month/:day' component={HelloWorld}/>
     <Route path='login' component={Login}/>
+    <Route path='about' component={About}/>
     <Route path='admin' component={Admin}/>
     <Route path='admin/events/:id' component={AdminEvent}/>
   </Route>
